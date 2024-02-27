b1e432c5f1f57210f091024aef4349bb048a904f
saeshaagarwal <110415532+saeshaagarwal@users.noreply.github.com>
2024-02-26 20:03:29 -0500

    merged

--- admin.js
+++ admin.js
@@ -1,29 +1,28 @@
-function domReady(fn) { 
-    if ( 
-        document.readyState === "complete" || 
+function domReady(fn) {
+    if (
+        document.readyState === "complete" ||
         document.readyState === "interactive"
-    ) { 
-        setTimeout(fn, 1000); 
-    } else { 
-        document.addEventListener("DOMContentLoaded", fn); 
-    } 
-} 
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
 
 let data = "";
-domReady(function () { 
-  
+domReady(function() {
+
     // If found you qr code 
-    function onScanSuccess(decodeText, decodeResult) { 
+    function onScanSuccess(decodeText, decodeResult) {
         // alert("You Qr is : " + decodeText, decodeResult);
         data = decodeText;
         createTable();
-    } 
-  
-    let htmlscanner = new Html5QrcodeScanner( 
-        "my-qr-reader", 
-        { fps: 10, qrbos: 250 } 
-    ); 
-    htmlscanner.render(onScanSuccess); 
+    }
+
+    let htmlscanner = new Html5QrcodeScanner(
+        "my-qr-reader", { fps: 10, qrbos: 250 }
+    );
+    htmlscanner.render(onScanSuccess);
 });
 
 function parseText() {
@@ -36,25 +35,78 @@ function parseText() {
 function createTable() {
     // Redo with for loops
     // Create .env file to store API link
-    // sketchy af rn
+    //The index of the nums in lines 53-60 & 66-73 are not sequential. Did this to put values extracted from QR code is a specific order in table.
+    //Do not change the ordering as this will change how data is displayed for user and also how data is sent to the spreadsheet. 
     document.getElementById("myTable").innerHTML = `
     <table style="font-family: arial, sans-serif; border-collapse: collapse; width: 100%;">
         <tr>
+            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Team Number</th>
             <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Match Number</th>
-            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Number of Cubes</th>
-            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Number of Cones</th>
+            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">In Our Alliance</th>
+        </tr>
+
+        <tr>
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">` + parseText()[0] + `</td>
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">` + parseText()[1] + `</td>
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">` + parseText()[6] + `</td>
+        </tr>
+
+        <tr>
+            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Left Community</th>
+            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Notes in Speaker</th>
+            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Notes in Amp</th>
+        </tr>
+
+        <tr>
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">` + parseText()[7] + `</td>
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">` + parseText()[2] + `</td>
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">` + parseText()[3] + `</td>
+        </tr>
+
+        <tr>
+            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Total Score</th>
+            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Auto Ranking</th>
+            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Complete Throw</th>
         </tr>
         <tr>
-            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">`+ parseText()[0]+ `</td>
-            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">`+ parseText()[1]+ `</td>
-            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">`+ parseText()[2]+ `</td>
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">` + parseText()[8] + `</td>
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">` + parseText()[4] + `</td>
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">` + parseText()[9] + `</td>
+        </tr>
+
+        <tr>
+            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Robot Parked</th>
+            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Where On Chain</th>
+            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">How Many Teams</th>
+        </tr>
+        <tr>
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">` + parseText()[10] + `</td>
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">` + parseText()[11] + `</td>
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">` + parseText()[5] + `</td>
+        </tr>
+
+        <tr>
+            <th style="border: 1px solid #dddddd; text-align: left; padding: 8px;">Score In Trap</th>
+        </tr>
+        <tr>
+            <td style="border: 1px solid #dddddd; text-align: left; padding: 8px;">` + parseText()[12] + `</td>
         </tr>
     </table>
 
     <form method="POST" action="https://script.google.com/macros/s/AKfycbyuop7CV1OKZhyZ-m1nqhgLdCMDOwluWWRIsVWVCMKKqxSE4YdOq8pYmuqC0efHHzkwPw/exec">
-        <input type="hidden" name="Match Number" value="`+ parseText()[0]+ `">
-        <input type="hidden" name="Number of Cones" value="`+ parseText()[1]+ `">
-        <input type="hidden" name="Number of Cubes" value="`+ parseText()[2]+ `">
+        <input type="hidden" name="Team Number" value="` + parseText()[0] + `">
+        <input type="hidden" name="Match Number" value="` + parseText()[1] + `">
+        <input type="hidden" name="In Our Alliance" value="` + parseText()[6] + `">
+        <input type="hidden" name="Left Community" value="` + parseText()[7] + `">
+        <input type="hidden" name="Notes in Speaker" value="` + parseText()[2] + `">
+        <input type="hidden" name="Notes in Amp" value="` + parseText()[3] + `">
+        <input type="hidden" name="Total Score" value="` + parseText()[8] + `">
+        <input type="hidden" name="Ranking" value="` + parseText()[4] + `">
+        <input type="hidden" name="Complete Throw" value="` + parseText()[9] + `">
+        <input type="hidden" name="Robot Parked" value="` + parseText()[10] + `">
+        <input type="hidden" name="Where On Chain" value="` + parseText()[11] + `">
+        <input type="hidden" name="How Many Teams" value="` + parseText()[5] + `">
+        <input type="hidden" name="Score In Trap" value="` + parseText()[12] + `">
         <button type="submit">Send to Spreadsheet</button>
     </form>
   `;
--- user.js
+++ user.js
@@ -1,37 +1,103 @@
-// Array with stuff
-const questions = ["match", "cones", "cubes"]
+//Global Variables that store the value at the end of the program. 
 
-// Annoying button logic
-function addition(element) {
-    // alert(element.id);
-    document.getElementById(element.id + "input").value = parseInt(document.getElementById(element.id + "input").value) + 1;
+var autoScore = 0;
+var leftCommunityPoints = 0;
+var leftCommunityString = "";
+var inAllianceString = "";
+var completethrowstring = "";
+var parkingstring = "";
+var trapscorestring = "";
+var whereonchainstring = "";
+var coopertitionString = "";
+var highlightedButtons = {};
+
+const questions = ["team", "match", "speaker", "amp", "speaker2", "apm2", "speed", "ranking", "hang"];
+
+//Get value of clicked buttons from html and assign similar values to variables - leftCommunity() and inAlliance()
+//The call to updateTotalScore() at the end of most of these methods is needed to calculate total auto score correctly
+//Used ternary operators for leftCommunity() and inAlliance() instead of using if blocks. Both have same functionality but ternary looks cleaner
+//Only use ternary when there are two possible vals. This means when there are 3 or more buttons in one section on the website, it will be hard to use ternary
+
+function leftCommunity(element) {
+    //Assigns two points if robot leaves community section
+    leftCommunityPoints = element.includes("yes") === true ? 2 : 0;
+    leftCommunityString = element.includes("yes") === true ? "Yes" : "No";
+    updateTotalScore();
+}
+
+function inAlliance(element) { inAllianceString = element.includes("yes") === true ? "Yes" : "No"; }
+
+function completeThrow(element) { completethrowstring = element.includes("yes") === true ? "Yes" : "No"; }
+
+function parking(element) { parkingstring = element.includes("yes") === true ? "Yes" : "No"; }
+
+function trapScore(element) { trapscorestring = element.includes("yes") === true ? "Yes" : "No"; }
+
+function whereOnChain(element) {
+    if (element.includes("left")) { whereonchainstring = "Left"; } else if (element.includes("right")) { whereonchainstring = "Right"; } else { whereonchainstring = "Didn't"; }
 }
 
 function subtraction(element) {
     document.getElementById(element.id + "input").value = parseInt(document.getElementById(element.id + "input").value) - 1;
+    updateTotalScore();
 }
 
+function addition(element) {
+    document.getElementById(element.id + "input").value = parseInt(document.getElementById(element.id + "input").value) + 1;
+    updateTotalScore();
+}
 
-// html is mid 
+//Goes through questions array and sets all values back to zero. Calls updateTotalScore() to change total score field on website to 0. 
+function clearForm() {
+    for (i = 0; i < questions.length; i++) document.getElementById(questions[i] + "input").value = 0;
+    updateTotalScore();
+}
+
+function coopertition(element) { coopertitionString = element.includes("yes") === true ? "Yes" : "No"; }
+
+
+//Unhighlights buttons. IMPORTANT: Does not change whether or not button has been actually clicked. Only changes Frontend(CSS)
+//Sets leftCommunityPoints to zero and calls updateTotalScore() to reset points for leaving community back to 0. 
+function unhighlightButtons() {
+    var buttons = document.querySelectorAll('.general-button, .num-button');
+    buttons.forEach(function(button) {
+        button.classList.remove('highlighted');
+    });
+    leftCommunityPoints = 0;
+    updateTotalScore();
+}
+
+//Changes the total score of auto dynamically on the website as user presses buttons
+function updateTotalScore() {
+    var speakerScore = parseInt(document.getElementById("speakerinput").value) * 5;
+    var ampScore = parseInt(document.getElementById("ampinput").value) * 2;
+
+    autoScore = speakerScore + ampScore + leftCommunityPoints;
+    document.getElementById("totalAutoScore").innerText = autoScore;
+}
+
+//highlights a button when pressed and unhighlights it when another button of the same section is pressed
+//To change the color the button gets highglighted to, go the user.css and alter rgb values of .highlighted block
+function highlightButton(sectionId, button) {
+    var currentHighlightedButton = highlightedButtons[sectionId];
+    if (currentHighlightedButton) { currentHighlightedButton.classList.remove('highlighted'); }
+    button.classList.add('highlighted');
+    highlightedButtons[sectionId] = button;
+}
+
 function getInputValue(element) {
     var inputVal = document.getElementById(element + "input").value;
-    // alert(inputVal);
     return inputVal;
-}
-
-function getQRCodeString() {
-    let qrCodeString = "";
-    for (i = 0; i < questions.length; i++) {
-        qrCodeString += getInputValue(questions[i]) + "|";
-    }
 }
 
 function generateQRCode() {
     let qrCodeString = "";
-    for (i = 0; i < questions.length; i++) {
-        qrCodeString += getInputValue(questions[i]) + "|";
-    }
-    // alert(qrCodeString);
+    for (i = 0; i < questions.length; i++) { qrCodeString += getInputValue(questions[i]) + "|"; }
+
+    //Using same QR Code logic of seperating all values with a pipe("|")
+    //Concatenating values to qrCodeString var simiilar to how it is done in for loop
+    qrCodeString += inAllianceString + "|" + leftCommunityString + "|" + autoScore + "|" + coopertitionString + "|" + completethrowstring + " | " + parkingstring + " | " + whereonchainstring + " | " + trapscorestring;
+
     document.getElementById("qrcode").innerHTML = "";
     new QRCode(document.getElementById("qrcode"), qrCodeString);
 }
